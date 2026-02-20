7a182def77dfdd3be2187dc282741d10d87fdd61
KoeterX <scotthanck@gmail.com>
2026-02-20 09:04:30 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,23 @@
-// Firebase configuratie - vervang met je eigen config
+// Firebase configuratie - Firebase v12
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAt-n2A_3FH9rY-XtsdfuHTgjtJQHIxqAY",
-  authDomain: "epsteinunited.firebaseapp.com",
-  projectId: "epstein-united", // <-- STREEPJE TERUGGEPLAATST!
-  storageBucket: "epsteinunited.appspot.com",
-  messagingSenderId: "603988196357",
-  appId: "1:603988196357:web:203e9451d880aafe63b06c",
-  measurementId: "G-BBSLNQFGH8"
+  apiKey: "AIzaSyBz2FL1s2d8KsYcH-QFMCx3RZWA_32BQUg",
+  authDomain: "epstein-united.firebaseapp.com",
+  projectId: "epstein-united",
+  storageBucket: "epstein-united.firebasestorage.app",
+  messagingSenderId: "1018415762457",
+  appId: "1:1018415762457:web:9492a507e5b0c612a0ae88",
+  measurementId: "G-FWZF62HZ2X"
 };
 
 // Initialiseer Firebase
-firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
-const auth = firebase.auth();
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
 
 // Export voor gebruik in andere files
 window.firebaseDB = db;
-window.firebaseAuth = auth;
+window.firebaseApp = app;
