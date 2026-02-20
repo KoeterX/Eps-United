6ac6679bb477028cb5a1873a3d15358834228163
KoeterX <scotthanck@gmail.com>
2026-02-20 09:29:07 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-// Firebase configuratie - EpsteinUnited project (Firebase v9.6.1 compat)
+// Firebase configuratie - EpsteinUnited project
 const firebaseConfig = {
     apiKey: "AIzaSyAt-n2A_3FH9rY-XtsdfuHTgjtJQHIxqAY",
     authDomain: "epsteinunited.firebaseapp.com",
@@ -16,25 +16,3 @@ const auth = firebase.auth();
 // Export voor gebruik in andere files
 window.firebaseDB = db;
 window.firebaseAuth = auth;
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-analytics.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBz2FL1s2d8KsYcH-QFMCx3RZWA_32BQUg",
-  authDomain: "epstein-united.firebaseapp.com",
-  projectId: "epstein-united",
-  storageBucket: "epstein-united.firebasestorage.app",
-  messagingSenderId: "1018415762457",
-  appId: "1:1018415762457:web:9492a507e5b0c612a0ae88",
-  measurementId: "G-FWZF62HZ2X"
-};
-
-// Initialiseer Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const analytics = getAnalytics(app);
-
-// Export voor gebruik in andere files
-window.firebaseDB = db;
-window.firebaseApp = app;
